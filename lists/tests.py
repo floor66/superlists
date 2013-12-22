9d70f63c68c069349b9e9d5ca877432275ae1ccb
floor66 <fpjdenhartog@gmail.com>
2013-12-22 17:45:47 +0100

    Created list item model, passed creating/saving unit test

--- lists/tests.py
+++ lists/tests.py
@@ -2,9 +2,28 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-
 from lists.views import home_page
+from lists.models import Item
 
+class ItemModelTest(TestCase):
+	
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'First created item'
+		first_item.save()
+		
+		second_item = Item()
+		second_item.text = 'Second created item'
+		second_item.save()
+		
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+		
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		
+		self.assertEqual(first_saved_item.text, 'First created item')
+		self.assertEqual(second_saved_item.text, 'Second created item')
 
 class HomePageTest(TestCase):
 
