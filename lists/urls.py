28bc963fbc989dceb08875540afa74df996fabe8
floor66 <fpjdenhartog@gmail.com>
2013-12-23 00:49:50 +0100

    Split up main app's urls.py and list app's urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -5,6 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
-	url(r'^lists/', include('lists.urls')),
+	url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+	url(r'^(\d+)/new_item/$', 'lists.views.new_item', name='new_item'),
+	url(r'^new/$', 'lists.views.new_list', name='new_list'),
+
     # url(r'^admin/', include(admin.site.urls)),
 )
