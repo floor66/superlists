0103a676d2362773baf32d1b73c80690945e6afe
floor66 <fpjdenhartog@gmail.com>
2013-12-31 15:44:43 +0100

    Adjusted new list view in order to cope with ValidationErrors

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 class ListAndItemModelTest(TestCase):
 	
@@ -31,4 +32,10 @@ class ListAndItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Second created item')
 		self.assertEqual(second_saved_item.list, list_)
+		
+	def test_cannot_save_empty_list(self):
+		list_ = List.objects.create()
+		item = Item(list=list_, text='')
+		with self.assertRaises(ValidationError):
+			item.save()
 	
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 def home_page(request):
@@ -12,7 +13,13 @@ def view_list(request, list_id):
 
 def new_list(request):
 	list_ = List.objects.create()
-	Item.objects.create(text=request.POST['item_text'], list=list_)
+	try:
+		Item.objects.create(text=request.POST['item_text'], list=list_)
+	except ValidationError:
+		error_text = 'You can\'t have an empty list item'
+		return render(request, 'home.html', {
+			'error': error_text
+		})
 	return redirect('/lists/%d/' % (list_.id,))
 	
 def new_item(request, list_id):
