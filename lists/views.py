84cb3a0707b44b38a2bb542a8c74493d522ae811
floor66 <fpjdenhartog@gmail.com>
2014-01-02 23:03:38 +0100

    Refactored hardcoded URLs out of views with get_absolute_url on the List model definition

--- lists/views.py
+++ lists/views.py
@@ -13,7 +13,7 @@ def view_list(request, list_id):
 	if request.method == 'POST':
 		try:
 			Item.objects.create(text=request.POST['item_text'], list=list_)
-			return redirect('/lists/%d/' % (list_.id,))
+			return redirect(list_)
 		except ValidationError:
 			error = 'You can\'t have an empty list item'
 	
@@ -33,5 +33,5 @@ def new_list(request):
 			'error': error
 		})
 		
-	return redirect('/lists/%d/' % (list_.id,))
+	return redirect(list_)
 	
