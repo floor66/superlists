2f832550456f89049277afb8dc9401cc0c84349c
floor66 <fpjdenhartog@gmail.com>
2014-01-05 13:00:08 +0100

    Added new form for adding to existing lists, implemented duplicate item validation into it

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 
 from lists.models import Item, List
-from lists.forms import ItemForm
+from lists.forms import (
+	EMPTY_LIST_ERROR, DUPLICATE_ITEM_ERROR,
+	ItemForm, ExistingListItemForm
+)
 
 class ItemFormTest(TestCase):
 	
@@ -17,7 +20,7 @@ class ItemFormTest(TestCase):
 		self.assertFalse(form.is_valid())
 		self.assertEqual(
 			form.errors['text'],
-			['You can\'t have an empty list item']
+			[EMPTY_LIST_ERROR]
 		)
 		
 	def test_form_save_handles_saving_to_a_list(self):
@@ -31,5 +34,32 @@ class ItemFormTest(TestCase):
 		self.assertEqual(item_.text, 'Save this')
 		self.assertEqual(item_.list, list_)
 		
+class ExistingListItemFormTest(TestCase):
+
+	def test_form_renders_item_text_input(self):
+		form = ItemForm()
+		self.assertIn('placeholder="Enter a To-Do item"', form.as_p())
+		self.assertIn('class="form-control input-lg"', form.as_p())
 		
-	
+	def test_form_validation_for_blank_items(self):
+		form = ItemForm(data={
+			'text': ''
+		})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(
+			form.errors['text'],
+			[EMPTY_LIST_ERROR]
+		)
+		
+	def test_form_validation_for_duplicate_items(self):
+		list_ = List.objects.create()
+		Item.objects.create(list=list_, text='no doubles')
+		form = ExistingListItemForm(for_list=list_, data={
+			'text': 'no doubles'
+		})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(
+			form.errors['text'],
+			[DUPLICATE_ITEM_ERROR]
+		)
+		
