5b670008c900c61f2b7e4a45db8fe6324574b0f5
floor66 <fpjdenhartog@gmail.com>
2014-01-05 17:11:39 +0100

    Finally fixed the viewing/adding list items in views.py with a CBGV. TIP: NAMED GROUP 'pk' IN urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from lists.views import NewListView, view_list
+from lists.views import NewListView, ViewAndAddToListView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^(\d+)/$', view_list, name='view_list'),
+	url(r'^(?P<pk>\d+)/$', ViewAndAddToListView.as_view(), name='view_list'),
 	url(r'^new$', NewListView.as_view(), name='new_list'),
 
     # url(r'^admin/', include(admin.site.urls)),
--- lists/views.py
+++ lists/views.py
@@ -18,20 +18,7 @@ class ViewAndAddToListView(CreateView):
 	def get_form(self, form_class):
 		self.object = self.get_object()
 		return form_class(for_list=self.object, data=self.request.POST)
-	
-def view_list(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	form = ExistingListItemForm(for_list=list_, data=request.POST or None)
-	
-	if form.is_valid():
-		form.save()
-		return redirect(list_)
-	
-	return render(request, 'list.html', {
-		'list': list_,
-		'form': form
-	})
-
+		
 class NewListView(CreateView, HomePageView):
 
 	def form_valid(self, form):
