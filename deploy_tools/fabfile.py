81fcfd0586a65d46a0642fee200964547a92ca94
floor66 <fpjdenhartog@gmail.com>
2014-01-06 16:05:37 +0100

    Added nginx/upstart config, running both services to fabfile.py

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -18,6 +18,9 @@ def deploy():
 	_update_virtualenv(source_folder)
 	_update_static_files(source_folder)
 	_update_database(source_folder)
+	_configure_nginx(source_folder)
+	_configure_upstart(source_folder)
+	_start_server()
 
 def _create_directory_structure_if_necessary(site_name):
 	base_folder = path.join(SITES_FOLDER, site_name).replace('\\', '/')
@@ -32,6 +35,7 @@ def _get_latest_source(source_folder):
 		run('cd %s && git fetch' % (source_folder,))
 	else:
 		run('git clone %s %s' % (REPO_URL, source_folder,))
+	
 	current_commit = local('git log -n 1 --format=%H', capture=True)
 	run('cd %s && git reset --hard %s' % (source_folder, current_commit,))
 	
@@ -39,6 +43,7 @@ def _update_settings(source_folder, site_name):
 	settings_path = path.join(source_folder, 'superlists/settings.py').replace('\\', '/')
 	sed(settings_path, 'DEBUG = True', 'DEBUG = False')
 	append(settings_path, 'ALLOWED_HOSTS = [\'%s\']' % (site_name,))
+	
 	secret_key_file = path.join(source_folder, 'superlists/secret_key.py').replace('\\', '/')
 	if not exists(secret_key_file):
 		chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
@@ -57,4 +62,22 @@ def _update_static_files(source_folder):
 	
 def _update_database(source_folder):
 	run('cd %s && ../virtualenv/bin/python3 manage.py syncdb --noinput' % (source_folder,))
+	
+def _configure_nginx(source_folder):
+	nginx_conf_file = '/etc/nginx/sites-available/%s' % (env.host,)
+	if not exists(nginx_conf_file):
+		run('sudo sed "s/SITENAME/%s/g" %s/deploy_tools/nginx.template.conf | sudo tee /etc/nginx/sites-available/%s' % (env.host, source_folder, env.host,))
+	
+	nginx_conf_file = '/etc/nginx/sites-enabled/%s' % (env.host,)
+	if not exists(nginx_conf_file):
+		run('sudo ln -s /etc/nginx/sites-available/%s /etc/nginx/sites-enabled/%s' % (env.host, env.host,))
+	
+def _configure_upstart(source_folder):
+	upstart_conf_file = '/etc/init/gunicorn-%s' % (env.host,)
+	if not exists(upstart_conf_file):
+		run('sudo sed "s/SITENAME/%s/g" %s/deploy_tools/gunicorn-upstart.template.conf | sudo tee /etc/init/gunicorn-%s.conf' % (env.host, source_folder, env.host,))
+	
+def _start_server():
+	run('sudo service nginx reload')
+	run('sudo start gunicorn-%s' % (env.host,))
 	
